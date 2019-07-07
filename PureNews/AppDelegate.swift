fe03c44945209f59ea08dc9822634aed46f3359f
张全鹏 <274430533@qq.com>
2019-07-07 16:06:01 +0800

    first commit

---
+++ PureNews/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PureNews
+//
+//  Created by ZQP on 2019/7/7.
+//  Copyright © 2019年 max. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
